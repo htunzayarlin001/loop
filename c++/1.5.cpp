2d4561c43cbab449f0ae9c998d0adba257c8603f
htunzayarlin001 <684244111@mail.pbru.ac.th>
2025-10-30 11:41:27 +0700

    Add files via upload

---
+++ c++/1.5.cpp
@@ -0,0 +1,43 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+
+  char op;
+  double num1, num2;
+
+  cout << "Enter the operator( +, -, *, /): ";
+  cin >> op;
+
+  cout << "Enter two two numbers one by one: ";
+  cin >> num1 >> num2;
+
+  switch(op) {
+
+    case '+':
+      cout << num1 << " + " << num2 << " = " << (num1 + num2);
+      break;
+
+    case '-':
+      cout << num1 << " - " << num2 << " = " << (num1 - num2);
+      break;
+
+    case '*':
+      cout << num1 << " * " << num2 << " = " << (num1 * num2);
+      break;
+
+    case '/':
+    	if (num2=!0.0)
+         cout << num1 << " / " << num2 << " = " << (num1 / num2);
+        else 
+         cout <<"Divide by zero operation";
+         
+      break;
+
+    default:
+      cout <<op<< "is an invalid operator";
+      break;
+  }
+
+  return 0;
+}
